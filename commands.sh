39617d728d3d72d611beb333ac8d7f9a0e57fd10
Josephnp732 <josephnp732@gmail.com>
2020-07-12 15:37:42 -0400

    Solved Q4

--- commands.sh
+++ commands.sh
@@ -7,11 +7,14 @@ cd /usr/local/opt/hadoop/sbin
 # Put file into HDFS
 hadoop fs -put ~/Downloads /
 
-# Execute Question 1
+# Execute (Number of Accidents Per Month)
 hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q1.DriverClass /Accidents.csv /Q1_Output
 
-# Execute Question 2
+# Execute (Number of Accidents vs Hour of Day)
 hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q2.DriverClass /Accidents.csv /Q2_Output
 
-# Execuet Question 3
+# Execute (Number of Accidents Per Weekday)
 hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q3.DriverClass /Accidents.csv /Q3_Output
+
+# Execute (Number of Accidents Per Year)
+hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q4.DriverClass /Accidents.csv /Q4_Output
