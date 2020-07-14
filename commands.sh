38956d0c3203297766981b57815a30cad0bec89c
Josephnp732 <josephnp732@gmail.com>
2020-07-14 12:29:03 -0400

    Added Q13 (Percentage - Per Traffic Proximity)

--- commands.sh
+++ commands.sh
@@ -4,8 +4,9 @@
 cd /usr/local/opt/hadoop/sbin
 ./start-dfs.sh
 
-# Put file into HDFS
-hadoop fs -put ~/Downloads/Accidents.csv /
+# Put files into HDFS
+hadoop fs -put ~/Downloads/Accidents.csv / &&
+hadoop fs -put ~/Downloads/State_Data.csv /
 
 #-----------------------------------------------#
 
@@ -40,8 +41,12 @@ hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataF
 hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q10.DriverClass /Accidents.csv /Q10_Output &&
 
 # Divide file into partitions divided by state
-hadoop fs -put ~/Downloads/State_Data.csv / &&
-hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q11.DriverClass /Accidents.csv /State_Data.csv /Q11_Output
+hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q11.DriverClass /Accidents.csv /State_Data.csv /Q11_Output &&
 
 # Percentage Per US Timezone
-hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q12.DriverClass /Accidents.csv /Q12_Output
+hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q12.DriverClass /Accidents.csv /Q12_Output &&
+
+# Proximity to Traffic Object (Percentage / per all traffic)
+hadoop jar /Users/christy/Downloads/Projects/BigDataFinalProject/target/BigDataFinalProject-1.0-SNAPSHOT.jar com.hadoop.finalProject.Q13.DriverClass /Accidents.csv /Q13_Output
+
+#-----------------------------------------------#
---
+++ src/main/java/com/hadoop/finalProject/Q13/MapperClass.java
@@ -0,0 +1,36 @@
+package com.hadoop.finalProject.Q13;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapperClass extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    Text proximity = new Text();
+    IntWritable one = new IntWritable(1);
+
+    RowHeaderClass rhc = new RowHeaderClass();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        String[] tokens = value.toString().split(",");
+        if (!tokens[0].equals("ID")) {
+
+            for(int i = 32; i < 45; i++) {
+                if(tokens[i].equals("True")) {
+                    proximity.set(rhc.getRowName(i));
+                    context.write(proximity, one);
+                }
+            }
+        }
+    }
+
+}
